a8cbe397bfbdaf725421e84bf2f94662e1a84940
Jakob Fels <fels.jakob@gmail.com>
2021-12-01 21:47:09 +0100

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,42 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+
+    fun part1(input: List<String>): SonarReport {
+        return input.map { it.toInt() }
+            .fold(SonarReport(-1, 0)) { prevReport, measurement ->
+            prevReport.addMeasurement(measurement)
+        }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): SonarReport {
+        return input.map { it.toInt() }
+            .windowed(3)
+            .fold(SonarReport(-1, 0)) { prevReport, measurement ->
+            prevReport.addMeasurement(measurement.sum())
+        }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput).increases == 7)
+    check(part2(testInput).increases == 5)
 
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
+}
+
+class SonarReport(val increases: Int = 0, private val measurement: Int) {
+
+    fun addMeasurement(newMeasurement: Int): SonarReport {
+        return if (newMeasurement > this.measurement) {
+            SonarReport(increases + 1, newMeasurement)
+        } else {
+            SonarReport(increases, newMeasurement)
+        }
+    }
+
+    override fun toString(): String {
+        return "SonarReport(increases=$increases, measurement=$measurement)"
+    }
+
 }
